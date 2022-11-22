967f58cf4a8afb3b3edf60b4cc94d8f4f846c63c
Dardo Lopez <da.lopez84@gmail.com>
2022-11-22 20:07:15 -0300

    primerentrega

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { BodyComponent } from './body/body.component';
+import { CardsComponent } from './cards/cards.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [				AppComponent,
+      MenuComponent,
+      ToolbarComponent,
+      BodyComponent,
+      CardsComponent
+   ],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
